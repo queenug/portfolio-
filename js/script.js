8aee1a531a4bfe4b814d5bd0b9abaf6ce8dba562
queenug <98647509+queenug@users.noreply.github.com>
2023-01-21 06:12:33 +0100

    Add files via upload
    Add initial portfolio website files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector("navbar");
+
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navbar.classList.remove("active")
+}
